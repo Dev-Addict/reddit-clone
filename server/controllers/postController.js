f410d7f8c84419ce2d472fe5aea57e53522589bb
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-29 20:05:19 +0430

    create post controller

---
+++ server/controllers/postController.js
@@ -0,0 +1,12 @@
+const factory = require('./handlerFactory');
+const Post = require('../models/Post');
+
+exports.getCommunities = factory.getAll(Post);
+
+exports.createPost = factory.createOne(Post);
+
+exports.getPost = factory.getOne(Post);
+
+exports.updatePost = factory.updateOne(Post);
+
+exports.deletePost = factory.deleteOne(Post);
